f0aa1ba0dcda04eaef5676fd439f74178a50565b
Jacu <j.smetek1@gmail.com>
2019-03-08 20:15:57 +0100

    Accessing a Server

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,47 @@
+import React,{ Component } from 'react';
+
+import Modal from '../../components/UI/Modal/Modal';
+import RAux from '../ReactAux/RAux'
+
+const withErrorHandler = (WrappedComponent, axios) => {
+    return class extends Component {
+        state = {
+            error: null
+        }
+        componentWillMount() {
+            this.reqInterceceptor = axios.interceptors.request.use(req => {
+                this.setState({error: null});
+                return req;
+            });
+            this.resInterceceptor = axios.interceptors.response.use(res => res,error => {
+                this.setState({error: error});
+            });
+        }
+
+        componentWillUnmount() {
+            console.log('Will Unmount', this.reqInterceceptor, this.resInterceceptor);
+            axios.interceptors.request.eject(this.reqInterceceptor);
+            axios.interceptors.response.eject(this.resInterceceptor);
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({error: null});
+        }
+
+        render() {
+            return (
+                <RAux>
+                    <Modal 
+                        show={this.setState.error}
+                        modalClosed={this.errorConfirmedHandler}>
+                        {this.state.error ? this.state.error.message : null}
+                        Something didn't work
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </RAux>
+            )
+        }
+    }
+}
+
+export default withErrorHandler;
